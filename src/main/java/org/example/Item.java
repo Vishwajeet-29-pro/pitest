42843f1e4c496fef2eb5ef831608beb8daca98dd
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-08-04 10:20:18 +0530

    refactor: With pitest report made changes in test cases and also fix wrong logic from Item class.

--- src/main/java/org/example/Item.java
+++ src/main/java/org/example/Item.java
@@ -15,11 +15,11 @@ public class Item {
     }
 
     public void updateQuality() {
-        if (!isSpecialItem()){
-            removeQuality();
-        } else {
+        if (isSpecialItem()) {
             addQuality();
             handleSpecialItems();
+        } else {
+            removeQuality();
         }
         decreaseSellIn();
         handleExpiredItems();
@@ -27,27 +27,31 @@ public class Item {
     }
 
     private void handleExpiredItems() {
-        if (sellIn < 0){
-            if (name.equals("Aged Brie")){
-                removeQuality();
+        if (sellIn < 0) {
+            if (name.equals("Aged Brie")) {
+                addQuality(); // Quality increases for Aged Brie
             } else if (name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                quality = 0;
+                quality = 0; // Quality drops to 0 after concert
             } else {
-                decreaseSellIn();
+                removeQuality(); // Decrease quality for normal items after expiration
             }
         }
     }
 
     private void decreaseSellIn() {
-        if (!name.equals("Sulfuras, Hand of Ragnaros")){
+        if (!name.equals("Sulfuras, Hand of Ragnaros")) {
             sellIn--;
         }
     }
 
     private void handleSpecialItems() {
-        if (name.equals("Backstage passes to a TAFKAL80ETC concert")){
-            if (sellIn < 11) addQuality();
-            if (sellIn < 6) addQuality();
+        if (name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (sellIn < 11) {
+                addQuality();
+            }
+            if (sellIn < 6) {
+                addQuality();
+            }
         }
     }
 
@@ -64,8 +68,8 @@ public class Item {
         return name.equals("Aged Brie") || name.equals("Backstage passes to a TAFKAL80ETC concert");
     }
 
-   @Override
-   public String toString() {
+    @Override
+    public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
     }
 }
